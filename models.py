39703d3897c8126c59e5ed47f8c8fea53a2faee3
Hans Benson L. Dy Tuan <hansbenson.dytuan@g.msuiit.edu.ph>
2023-10-25 15:13:37 +0800

    Modified models.py

--- models.py
+++ models.py
@@ -2,76 +2,53 @@ from app import mysql
 
 #from werkzeug.security import generate_password_hash, check_password_hash
 
-def getlatest():
-    cursor = mysql.connection.cursor()
-    sql = f"SELECT id_number FTOM students ORDER BY id_number DESC LIMIT 1"
-    cursor.execute(sql)
-    ids = cursor.fetchall()
-
-    return ids
-
 
 class Students(object):
 
-    def __init__(self, id = None, first = None, last = None, course = None,
-                 year = None, gender = None, upload = None, oldid = None):
+    def __init__(self, id = None, firstname = None, lastname = None, course_code = None,
+                 year = None, gender = None, oldid = None):
         self.id = id
-        self.first = first
-        self.last = last
-        self.course = course
+        self.firstname = firstname
+        self.lastname = lastname
+        self.course_code = course_code
         self.year = year
         self.gender = gender
         self.oldid = oldid
 
-    def addstudent(self):
+    def addStudent(self):
         cursor = mysql.connection.cursor()
-        sql = f"INSERT INTO students(id_number,first_name,last_name,course,year_level,gender,image) \
-                VALUES('{self.id}','{self.first}','{self.last}','{self.course}','{self.year}','{self.gender}', '{self.upload}')"
+        sql = f"INSERT INTO student(id,firstname,lastname,course_code,year,gender) \
+                VALUES('{self.id}','{self.firstname}','{self.lastname}','{self.course_code}','{self.year}','{self.gender}')"
         print(sql)
         cursor.execute(sql)
         mysql.connection.commit()
 
-    def editstudent(self):
+    def editStudent(self):
         cursor = mysql.connection.cursor()
         print(self.id)
-        if self.upload:
-            sql = f"UPDATE students SET id_number ='{self.id}',first_name ='{self.first}',last_name ='{self.last}'," \
-                  f"course ='{self.course}',year_level ='{self.year}'," \
-                  f"gender ='{self.gender}', image ='{self.upload}' WHERE id_number = '{self.oldid}'"
-            print(sql, "With Image")
-        else:
-            sql = f"UPDATE students SET id_number ='{self.id}',first_name ='{self.first}',last_name ='{self.last}'," \
-                  f"course ='{self.course}',year_level ='{self.year}'," \
-                  f"gender ='{self.gender}' WHERE id_number = '{self.oldid}'"
-            print(sql, "Without Image")
+
+        sql = f"UPDATE student SET id ='{self.id}',firstname ='{self.firstname}',lastname ='{self.lastname}'," \
+                f"course_code ='{self.course_code}',year_level ='{self.year}'," \
+                f"gender ='{self.gender}' WHERE id_number = '{self.oldid}'"
         try:
             cursor.execute(sql)
         except Exception as e:
             print(e)
         mysql.connection.commit()
 
-
     @classmethod
-    def allstudent(cls):
+    def studentData(cls):
         cursor = mysql.connection.cursor()
-        sql = "SELECT * from students"
+        sql = "SELECT * from student"
         cursor.execute(sql)
         result = cursor.fetchall()
         return result
 
     @classmethod
-    def datastudent(cls):
-        cursor = mysql.connection.cursor()
-        sql = "SELECT * from students"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        return result
-
-    @classmethod
-    def deletestudent(cls, id):
+    def deleteStudent(cls, id):
         try:
             cursor = mysql.connection.cursor()
-            sql = f"DELETE from students where id_number = '{id}'"
+            sql = f"DELETE from student where id = '{id}'"
             cursor.execute(sql)
             mysql.connection.commit()
             return True
@@ -79,52 +56,42 @@ class Students(object):
             print(e)
             return False
 
-    def searchstudent(cls, id):
+    def searchStudent(cls, id):
         cursor = mysql.connection.cursor()
-        sql = f"SELECT * from students where id_number='{id}'"
+        sql = f"SELECT * from student where id='{id}'"
         cursor.execute(sql)
         ids = cursor.fetchall()
         return ids
 
 class Courses(object):
 
-    def __init__(self, code = None, name = None, college = None, oldcode = None):
+    def __init__(self, code = None, name = None, college_code = None, oldcode = None):
         self.code = code
         self.name = name
-        self.college = college
+        self.college_code = college_code
         self.oldcode = oldcode
 
-    def addcourse(self):
+    def addCourse(self):
         cursor = mysql.connection.cursor()
-        sql = f"INSERT INTO course(course_code,course_name,course_college) \
-                VALUES('{self.code}','{self.name}','{self.college}')"
+        sql = f"INSERT INTO course(code,name,college_code) \
+                VALUES('{self.code}','{self.name}','{self.college_code}')"
         print(sql)
         cursor.execute(sql)
         mysql.connection.commit()
 
-    def editcourse(self):
+    def editCourse(self):
         cursor = mysql.connection.cursor()
         print(self.code)
-        sql = f"UPDATE course SET course_code ='{self.code}',course_name ='{self.name}',course_college ='{self.college}'" \
-              f"WHERE course_code = '{str(self.oldcode)}'"
+        sql = f"UPDATE course SET code ='{self.code}', name ='{self.name}',college_code ='{self.college_code}'" \
+              f"WHERE code = '{str(self.oldcode)}'"
         try:
             cursor.execute(sql)
         except Exception as e:
             print(e)
         mysql.connection.commit()
 
-
     @classmethod
-    def allcourse(cls):
-        cursor = mysql.connection.cursor()
-
-        sql = "SELECT * from course"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        return result
-
-    @classmethod
-    def datacourse(cls):
+    def courseData(cls):
         cursor = mysql.connection.cursor()
         sql = "SELECT * from course"
         cursor.execute(sql)
@@ -132,10 +99,10 @@ class Courses(object):
         return result
 
     @classmethod
-    def deletecourse(cls, code):
+    def deleteCourse(cls, code):
         try:
             cursor = mysql.connection.cursor()
-            sql = f"DELETE from course where course_code = '{code}'"
+            sql = f"DELETE FROM course where code = '{code}'"
             cursor.execute(sql)
             mysql.connection.commit()
             return True
@@ -143,32 +110,32 @@ class Courses(object):
             print(e)
             return False
 
-    def searchcourse(cls, code):
+    def searchCourse(cls, code):
         cursor = mysql.connection.cursor()
-        sql = f"SELECT * from course where course_code='{code}'"
+        sql = f"SELECT * from course where code='{code}'"
         cursor.execute(sql)
         codes = cursor.fetchall()
         return codes
 
 class College(object):
 
-    def __init__(self, codec = None, namec = None, oldcodec = None):
-        self.codec = codec
-        self.namec = namec
-        self.oldcodec = oldcodec
+    def __init__(self, code = None, name = None, oldcode = None):
+        self.code = code
+        self.name = name
+        self.oldcode = oldcode
 
-    def addcollege(self):
+    def addCollege(self):
         cursor = mysql.connection.cursor()
-        sql = f"INSERT INTO college(college_code,college_name) \
-                VALUES('{self.codec}','{self.namec}')"
+        sql = f"INSERT INTO college(code, name) \
+                VALUES('{self.code}','{self.name}')"
         print(sql)
         cursor.execute(sql)
         mysql.connection.commit()
 
-    def editcollege(self):
+    def editCollege(self):
         cursor = mysql.connection.cursor()
-        print(self.codec)
-        sql = f"UPDATE college SET college_code ='{self.codec}',college_name ='{self.namec}' WHERE college_code = '{str(self.oldcodec)}'"
+        print(self.code)
+        sql = f"UPDATE college SET code ='{self.code}', name ='{self.name}' WHERE code = '{str(self.oldcode)}'"
         print(sql)
         try:
             cursor.execute(sql)
@@ -176,19 +143,8 @@ class College(object):
             print(e)
         mysql.connection.commit()
 
-
     @classmethod
-    def allcollege(cls):
-        cursor = mysql.connection.cursor()
-
-        sql = "SELECT * from college"
-        print(sql, "ALL COLLEGE SQL")
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        return result
-
-    @classmethod
-    def datacollege(cls):
+    def collegeData(cls):
         cursor = mysql.connection.cursor()
         sql = "SELECT * from college"
         cursor.execute(sql)
@@ -196,11 +152,11 @@ class College(object):
         return result
 
     @classmethod
-    def deletecollege(cls, codec):
+    def deleteCollege(cls, code):
         try:
-            print(codec, "codec delete")
+            print(code, "code delete")
             cursor = mysql.connection.cursor()
-            sql = f"DELETE from college where college_code = '{codec}'"
+            sql = f"DELETE from college where code = '{code}'"
             cursor.execute(sql)
             mysql.connection.commit()
             return True
@@ -208,9 +164,9 @@ class College(object):
             print(e)
             return False
 
-    def searchcollege(cls, codec):
+    def searchCollege(cls, code):
         cursor = mysql.connection.cursor()
-        sql = f"SELECT * from college where college_code='{codec}'"
+        sql = f"SELECT * from college where code='{code}'"
         print(sql, "SEARCH COLLEGE")
         cursor.execute(sql)
         codec = cursor.fetchall()
